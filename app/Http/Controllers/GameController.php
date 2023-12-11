bec19156e7232ab4699661480ce5150029ee59c6
Kirill <flash27.01@mail.ru>
2023-12-11 21:49:00 +0600

    New controllers

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,25 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Game;
 
 class GameController extends Controller
 {
     // Выбор игры
-    public function chooseGame(Request $request) {
-        // Валидация данных (если необходимо)
-        // ...
-
+    public function chooseGame(Request $request)
+    {
         // Получение выбора пользователя
         $selectedGame = $request->input('game');
 
-        // Дополнительная логика, если необходимо
+        // Вывод списка игр из базы данных
+        $games = Game::all();
 
         // Перенаправление на страницу выбранной игры
         if ($selectedGame == '2048') {
             return redirect('/2048')->with('info', 'Добро пожаловать в игру 2048!');
         } elseif ($selectedGame == 'sudoku') {
             return redirect('/sudoku')->with('info', 'Добро пожаловать в игру Судоку!');
-        } 
+        }
+    }
+
+    // Страница игры 2048
+    public function play2048()
+    {
+        // Логика для отображения страницы игры 2048
+        return view('2048'); // Предполагается, что у вас есть шаблон Blade для этой страницы
+    }
+
+    // Страница игры Судоку
+    public function playSudoku()
+    {
+        // Логика для отображения страницы игры Судоку
+        return view('sudoku'); // Предполагается, что у вас есть шаблон Blade для этой страницы
     }
 }
-
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -25,10 +26,35 @@ class UserController extends Controller
         // Создание нового пользователя
         $user = User::create([
             'username' => $request->input('username'),
-            'password' => bcrypt($request->input('password')),
+            'password' => hash($request->input('password')),
         ]);
 
         // Отправка успешного ответа
         return response()->json(['message' => 'User registered successfully', 'user' => $user], 201);
+    }
+
+    // Авторизация пользователя
+    public function login(Request $request)
+    {
+        // Валидация входных данных с использованием Laravel's Validator
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        // Проверка наличия ошибок валидации
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        // Аутентификация пользователя
+        if (Auth::attempt(['username' => $request->input('username'), 'password' => $request->input('password')])) {
+            $user = Auth::user();
+            $token = $user->createToken('MyApp')->accessToken;
+
+            return response()->json(['message' => 'Login successful', 'user' => $user, 'access_token' => $token]);
+        } else {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
     }
 }
