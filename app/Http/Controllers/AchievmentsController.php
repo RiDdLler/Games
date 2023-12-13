c89481d4e41dad96c557a1f4d498bafba52c35ed
Kirill <flash27.01@mail.ru>
2023-12-13 22:13:15 +0600

    AddControllers

--- app/Http/Controllers/AchievmentsController.php
+++ app/Http/Controllers/AchievmentsController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserAchievement; // Assuming you have a model for UserAchievement
 
-class AchievmentsController extends Controller
+class AchievementsController extends Controller
 {
     // Проверка условий для выполнения достижений
     public function checkAchievements() {
@@ -12,7 +13,7 @@ class AchievmentsController extends Controller
         // и ваши условия для достижений задаются где-то в вашем приложении
 
         // Пример: Допустим, пользователь достиг высокого счета в игре "2048"
-        $highScore2048 = 10000; // Порог для высокого счета в "2048"
+        $highScore2048 = 2048; // Порог для высокого счета в "2048"
         $userScore2048 = 12000; // Полученный пользователем счет в "2048"
 
         // Проверка условий достижения для игры "2048"
@@ -23,11 +24,25 @@ class AchievmentsController extends Controller
                 'achievement' => 'High Score in 2048',
             ]);
 
-            // Дополнительная логика, например, отправка уведомления пользователю
-
-            return redirect('/dashboard')->with('success', 'Поздравляем! Вы достигли высокого счета в 2048!');
+            // Дополнительная логика, например, обновление интерфейса или запись в лог
         }
 
-        return redirect('/dashboard')->with('info', 'У вас пока нет новых достижений.');
+        // Добавляем условия для достижений в судоку
+        $sudokuAchievement = 5; // Порог для достижения в судоку
+        $userSudokuScore = 6; // Полученный пользователем счет в судоку
+
+        // Проверка условий достижения для игры в судоку
+        if ($userSudokuScore >= $sudokuAchievement) {
+            // Если условие выполнено, создаем запись о достижении пользователя
+            UserAchievement::create([
+                'user_id' => auth()->id(),
+                'achievement' => 'Sudoku Master',
+            ]);
+
+            // Дополнительная логика, например, обновление интерфейса или запись в лог
+        }
     }
 }
+
+//скрипт ачивок раз в ~10мин schedule
+//вывод чивок
--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -3,24 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\GameHistory;
 
-//use App\Models\GameHistory; // Подставьте вашу модель для истории игр
-
-class HistoryController extends Controller{
+class HistoryController extends Controller {
+    
     // Добавление данных об игре в базу данных
     public function addGameHistory(Request $request) {
-
+        
         // Получение данных об игре
-        $gameType = $request->input('game_type');
-        $score = $request->input('score');
+        $gameType = $request->game_type;
+        $score = $request->score;
 
         // Создание записи в таблице истории игр
-        GameHistory::create([
-            'user_id' => auth()->id(), // Предполагается, что у вас есть аутентификация
+        $GameHistory = GameHistory::create([
+            'user_id' => auth()->id(),
             'game_type' => $gameType,
             'score' => $score,
         ]);
 
-        return redirect('/dashboard')->with('success', 'Данные об игре успешно сохранены.');
+        return $GameHistory;
     }
 }
+//убрал input
